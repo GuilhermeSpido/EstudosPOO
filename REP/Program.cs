35eadc423f47c9ef747c35077085cfe7e18199ce
Guilherme Spido <spidoguilherme@gmail.com>
2025-06-19 16:52:12 -0300

    FuncionalidadeServicePessoa

--- REP/Program.cs
+++ REP/Program.cs
@@ -17,17 +17,15 @@ namespace REP
             Pessoa p2 = new Pessoa("Gui", 23);
             Pessoa p3 = new Pessoa("Gui", 23);
             Pessoa p4 = new Pessoa("Gui", 23);
-            Pessoa p5 = new Pessoa("Gui", 23);
+            Pessoa p5 = new Pessoa("Gui", 90);
 
             ServicePessoa servicePessoa = new ServicePessoa();
-            servicePessoa.Adicionar(p1);
-            servicePessoa.Adicionar(p2);
-            servicePessoa.Adicionar(p3);
-            servicePessoa.Adicionar(p4);
+            //servicePessoa.ListarMesmo();
+            //servicePessoa.Remover(p1);
+            //Console.WriteLine("REMOVIDO P1");
+            //servicePessoa.ListarMesmo();
+            servicePessoa.Salvar();
             servicePessoa.Adicionar(p5);
-            servicePessoa.ListarMesmo();
-            servicePessoa.Remover(p1);
-            Console.WriteLine("REMOVIDO P1");
             servicePessoa.ListarMesmo();
 
 
